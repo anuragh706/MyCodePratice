383a28c1813a724d6fa603cd04ef1471f36e79cb
Anuraag Shailendra Khandagle <anuragh706@gmail.com>
2023-05-18 23:57:10 +0100

    Substrings of a string

---
+++ Data Structures/Strings/18_SubstringsOfAString/main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+void PrintSubstring(string s) {
+	int n = s.length();
+	
+	for (int i = 0; i < n; i++) {
+	
+		string temp = "";
+		for (int j = i; j < n; j++) {
+			
+			temp += s[j];
+			cout << temp << endl;
+		}
+		//cout << temp << endl;
+
+	}
+
+}
+int main() {
+
+	string str = "abcd";
+	PrintSubstring(str);
+}
